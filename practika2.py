2bb7f3eba07736711408ea8a465d432fbbc38826
qwiio13 <xalyavkin.v@gmail.com>
2026-08-19 13:10:53 +0300

    Исправил проверку возраста в practika2

--- practika2.py
+++ practika2.py
@@ -8,7 +8,9 @@ class Animal:
         return self.__age
 
     def set_age(self, age):
-        if age > 0:
+        if not isinstance(age, int):
+            print('Возраст должен быть целым числом')
+        elif age > 0:
             self.__age = age
         else:
             print('Ошибка неправильный возраст!')
@@ -37,3 +39,16 @@ class Dog(Animal):
 
     def voice(self):
         print('Гав!')
+
+
+tom = Cat('Tom', 'brown')
+chop = Dog('Chop', 'buld')
+
+tom.voice()
+tom.purr()
+
+tom.get_age()
+tom.set_age(6)
+tom.get_age()
+tom.set_age(-7)
+tom.set_age(None)
